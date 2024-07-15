4f4e844e94feb3a1e7451a802271f0009afed87d
unknown <rahulkanjariya9265@gmail.com>
2024-07-15 11:18:28 +0530

    CheckBox and radiogroup and radiobutton

--- RelativeLayout_AllButtons/app/src/main/java/com/example/relativelayout_allbuttons/MainActivity.kt
+++ RelativeLayout_AllButtons/app/src/main/java/com/example/relativelayout_allbuttons/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         var tglbtn : ToggleButton = findViewById(R.id.toggleButton)
         var imgview : ImageView = findViewById(R.id.imageView)
         var  floatingbtn : FloatingActionButton = findViewById(R.id.floatingActionButton)
+//        var phone : EditText = findViewById(R.id.editTextPhone)
 
         button.setOnClickListener {
             Toast.makeText(applicationContext,"Simple Button Clicked..",Toast.LENGTH_LONG).show()
@@ -30,9 +31,49 @@ class MainActivity : AppCompatActivity() {
                 imgview.setImageResource(R.drawable.on)
             }
         }
+//        floatingbtn.setOnClickListener {
+//            val phoneNumber = phone.text.toString()
+//            if (phoneNumber.isNotEmpty()) {
+//                Toast.makeText(this, "Phone Number: $phoneNumber", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(this, "Please enter a phone number", Toast.LENGTH_SHORT).show()
+//            }
+//        }
 
-        floatingbtn.setOnClickListener {
-            Toast.makeText(applicationContext,"floating Button Clicked..",Toast.LENGTH_LONG).show()
+        var ch1 : CheckBox = findViewById(R.id.checkBox)
+        var ch2 : CheckBox = findViewById(R.id.checkBox1)
+        var ch3 : CheckBox = findViewById(R.id.checkBox2)
+        var str : String
+        var textview : TextView = findViewById(R.id.textView2)
+
+
+        ch1.setOnClickListener {
+             str = "Java : ${ch1.isChecked}\n Kotlin : ${ch2.isChecked}\nAndroid : ${ch3.isChecked}"
+            textview.setText(str)
+        }
+        ch2.setOnClickListener {
+             str = "Java : ${ch1.isChecked}\n Kotlin : ${ch2.isChecked}\nAndroid : ${ch3.isChecked}"
+            textview.setText(str)
+        }
+        ch3.setOnClickListener {
+             str = "Java : ${ch1.isChecked}\n Kotlin : ${ch2.isChecked}\nAndroid : ${ch3.isChecked}"
+            textview.setText(str)
+        }
+
+        var rg : RadioGroup = findViewById(R.id.ReadioGroup)
+        var tv2 : TextView = findViewById(R.id.textView3)
+        var resetBtn : Button = findViewById(R.id.resetbtn)
+
+        rg.setOnCheckedChangeListener { radioGroup, i ->
+            var rb = findViewById<RadioButton>(i)
+            if(rb!=null){
+                tv2.setText(rb.text)
+            }
+        }
+
+        resetBtn.setOnClickListener {
+            rg.clearCheck()
+            tv2.setText("Select options")
         }
     }
 }
