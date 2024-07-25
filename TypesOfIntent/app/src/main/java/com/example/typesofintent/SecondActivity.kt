758c829615a2619b6c099f203e5a2ad831c5f230
kanjariyarahul <rahulkanjariya9265@gmail.com>
2024-07-25 19:48:42 +0530

    intent code change

--- TypesOfIntent/app/src/main/java/com/example/typesofintent/SecondActivity.kt
+++ TypesOfIntent/app/src/main/java/com/example/typesofintent/SecondActivity.kt
@@ -1,11 +1,11 @@
 package com.example.typesofintent
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,23 +15,38 @@ class SecondActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
 
+        val textSurname: TextView = findViewById(R.id.textSurname)
+        val textName: TextView = findViewById(R.id.textName)
 
-        var textSurname : TextView = findViewById(R.id.textSurname)
-        var textName : TextView = findViewById(R.id.textName)
+        val intent: Intent = intent
+        val sName = intent.getStringExtra("surname")
+        val Name = intent.getStringExtra("name")
 
-        var intent: Intent = intent
-        var sName = intent.getStringExtra("surname" )
-        var Name = intent.getStringExtra("name" )
+        textSurname.text = "Surname : $sName"
+        textName.text = "Name : $Name"
 
-        textSurname.setText("Surname : "+ sName)
-        textName.setText("Name : " + Name )
-
-        var btn_web :Button = findViewById(R.id.button2)
-        btn_web.setOnClickListener {
-//            var uri = Uri.parse("Https://www.google.com")
+        val btnWeb: Button = findViewById(R.id.button2)
+        btnWeb.setOnClickListener {
+            val uri = Uri.parse("https://www.google.com") // Corrected the URL schema to lowercase
+            val i = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(i)
         }
 
+        var ed_number : EditText = findViewById(R.id.editTextPhone)
+        var btn_phone : Button = findViewById(R.id.button3)
 
+        btn_phone.setOnClickListener {
+            var uri = Uri.parse("tel : "+ed_number.text.toString())
+            var i = Intent(Intent.ACTION_DIAL,uri)
+            startActivity(i)
+        }
 
+        var btn_map : Button = findViewById(R.id.button4)
+
+        btn_map.setOnClickListener {
+            var uri = Uri.parse("geo:0,0=?"+ "jamnagr, gujarat")
+            var i = Intent(Intent.ACTION_VIEW,uri)
+            startActivity(i)
+        }
     }
 }
