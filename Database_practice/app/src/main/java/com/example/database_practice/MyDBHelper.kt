6f14e3446c7c83b80412862f794bf432ae41ed69
kanjariyarahul <rahulkanjariya9265@gmail.com>
2024-09-13 17:37:39 +0530

    chang

--- Database_practice/app/src/main/java/com/example/database_practice/MyDBHelper.kt
+++ Database_practice/app/src/main/java/com/example/database_practice/MyDBHelper.kt
@@ -1,4 +1,16 @@
 package com.example.database_practice
 
-class MyDBHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class MyDBHelper(context : Context) : SQLiteOpenHelper(context, "Student",null,1){
+    override fun onCreate(p0: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+
+    }
+
 }
